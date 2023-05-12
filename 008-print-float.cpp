589eb3dc9e3a59f7aa447e6d5e4ec9d997bdbddf
qinfeng <663250456@qq.com>
2023-05-12 17:00:36 +0800

    008 file add

---
+++ 008-print-float.cpp
@@ -0,0 +1,13 @@
+//
+// Created by lance on 23-5-12.
+//
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double x;
+    cin >> x;
+    cout << fixed << setprecision(12) << x << endl;
+    return 0;
+}
