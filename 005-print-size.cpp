754f4b3f8068efe545cf631ce7c5a50c9703d06a
qinfeng <663250456@qq.com>
2023-05-12 16:19:27 +0800

    005 file add

---
+++ 005-print-size.cpp
@@ -0,0 +1,13 @@
+//
+// Created by lance on 23-5-12.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int data = 1;
+    short int short_data = 2;
+    cout << sizeof(data) << " " << sizeof(short_data) << endl;
+    return 0;
+}
