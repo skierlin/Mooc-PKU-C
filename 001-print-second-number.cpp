24e72d15738c53ea56506b2491a2d20277db01b7
qinfeng <663250456@qq.com>
2023-05-12 15:29:10 +0800

    001 file add

---
+++ 001-print-second-number.cpp
@@ -0,0 +1,13 @@
+//
+// Created by lance on 23-5-12.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b ,c;
+    cin >> a >> b >> c;
+    cout << b << endl;
+    return 0;
+}
