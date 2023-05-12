cdde526aa1e1880d3fd0f0a40f3051df42c371df
qinfeng <663250456@qq.com>
2023-05-12 16:38:59 +0800

    006 file add

---
+++ 006-print-float-size.cpp
@@ -0,0 +1,14 @@
+//
+// Created by lance on 23-5-12.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float data = 1;
+    double double_data = 2;
+    cout << sizeof(data) << " " << sizeof(double_data) << endl;
+    return 0;
+}
